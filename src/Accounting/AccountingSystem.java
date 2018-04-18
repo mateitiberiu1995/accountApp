4596c6eb45b044ecfae02d4feb4cf66aa70c6cfe
mateitiberiu <mateitiberiu1995@gmail.com>
2018-04-18 12:48:51 +0100

    deleting commented code

--- src/Accounting/AccountingSystem.java
+++ src/Accounting/AccountingSystem.java
@@ -6,55 +6,7 @@ import java.util.Scanner;
 public class AccountingSystem {
 	private static HashMap<String, Account> accountsList = new HashMap<String, Account>();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*String answer;
-		Scanner reader = new Scanner(System.in);
-		answer=reader.next();
-		while(!answer.equals("stop"))
-		{
-			System.out.println(answer);
-			if(answer.equals("add"))
-			{
-				String firstName,lastName,accountNumber;
-				System.out.println("Write your first name: ");
-				firstName=reader.next();
-				System.out.println("Write your last name: ");
-				lastName=reader.next();
-				System.out.println("Write your account number: ");
-				accountNumber=reader.next();
-				if(add(firstName,lastName,accountNumber))
-				{
-					System.out.println("account added");
-				}
-				else {
-					System.out.println("The account is already in System!");
-				}
-			}
-			if(answer.equals("remove"))
-			{
-				String accountNumber;
-				System.out.println("Write your account number: ");
-				accountNumber=reader.next();
-				if(accountsList.containsKey(accountNumber))
-				{
-					accountsList.remove(accountNumber);
-				}
-				else {
-					System.out.println("The account is not in the System!");
-				}
-			}
-			if(answer.equals("show"))
-			{
-				for (String accountNumber: accountsList.keySet()){
-
-			        System.out.println(show(accountNumber));
-				} 
-			}
-			answer=reader.next();
-		}
 		
-		reader.close();*/
-
 	}
 	public static boolean add(String firstName,String lastName,String accountNumber)
 	{
@@ -75,16 +27,6 @@ public class AccountingSystem {
 
 	        return (accountsList.get(accountNumber).getFirst_name()+ " " + accountsList.get(accountNumber).getLast_name()+ " " + accountsList.get(accountNumber).getAccount_number());
 
-	}
-	public static boolean remove(String accountNumber)
-	{
-		if(accountsList.containsKey(accountNumber))
-		{
-			accountsList.remove(accountNumber);
-			return true;
-		}
-		else
-			return false;
 	}
 	
 	
